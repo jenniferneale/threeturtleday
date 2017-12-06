feb7e35f17caf2e1b5025c65480e423c8f95fdd9
jenniferneale <jeneale6@gmail.com>
2017-12-06 17:11:15 -0500

    A little debugging

--- server/db/blogpost.js
+++ server/db/blogpost.js
@@ -1,6 +1,6 @@
 'use strict';
 const { STRING, TEXT } = require('sequelize');
-const db = require('../index');
+const db = require('./index');
 // Require all the models
 // Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db so any other part of the application could call db.model('user') OR db.models.user to get access to the `user` model.
 // This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
--- server/db/seed.js
+++ server/db/seed.js
@@ -5,10 +5,7 @@ const { Blogpost } = require('./blogpost');
 db.sync({ force: true })
     .then(() => {
         Blogpost.bulkCreate([
-            { name: 'Terra', image: 'https://c2.staticflickr.com/4/3712/14294653894_62fc3498b7_b.jpg' },
-            { name: 'Luna', image: 'moon-2092807_1280.jpg' },
-            { name: 'Mars', image: 'https://upload.wikimedia.org/wikipedia/commons/7/77/Crossing_the_Aqueduct._-_geograph.org.uk_-_31580.jpg' },
-            { name: 'Titan', image: 'http://i.telegraph.co.uk/multimedia/archive/01107/aquarium_1107958i.jpg' }
+            { title: 'Testy', cardImage: 'https://c2.staticflickr.com/4/3712/14294653894_62fc3498b7_b.jpg' },            
         ]);
     })
     /*.then(() => {
